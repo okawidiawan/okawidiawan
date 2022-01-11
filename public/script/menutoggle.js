ccad0412350fe9341c5395826426a9279ce3ec0a
Oka Widiawan <oka.widiawan@gmail.com>
2022-01-12 04:16:25 +0700

    New version -  okawidiawan website v-2

--- public/script/menutoggle.js
+++ public/script/menutoggle.js
@@ -1,13 +1,10 @@
 export function close() {
   const menuToogle = document.querySelector(".menu-toggle");
+  const navBotLink = document.querySelectorAll(".navBotLink");
 
   menuToogle.addEventListener("click", () => {
-    const closeToggle = document.querySelectorAll(".menu-toggle span");
-    const menuBar = document.querySelector(".navigation");
-    closeToggle.forEach((element, index) => {
-      element.classList.toggle(`rotate-${index + 1}`);
+    navBotLink.forEach((element) => {
+      element.classList.toggle("show");
     });
-    menuBar.classList.toggle("show");
-    document.body.classList.toggle("no-scroll");
   });
 }
